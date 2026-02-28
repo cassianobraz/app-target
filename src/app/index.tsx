1b082e572740b9a4ab9139cab30fb6bf4ab9248e
Cassiano Pereira Braz <cassiano.braz@dcoimbra.com.br>
2026-02-28 11:53:30 -0400

    feat: three components and one layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,50 @@
-import { Text, View } from 'react-native';
+import { HomeHeader } from '@/components/HomeHeader';
+import { List } from '@/components/List';
+import { Target } from '@/components/Target';
+import { View } from 'react-native';
+
+const summary = {
+  total: "R$ 2.680,00",
+  input: { label: "Entradas", value: "R$ 6.184,90" },
+  output: { label: "Saídas", value: "-R$ 883.65" }
+}
+const targets = [
+  {
+    id: '1',
+    name: "Apple Watch",
+    percentage: "50%",
+    current: "R$ 580,00",
+    target: "R$ 1.790,00",
+  },
+  {
+    id: '2',
+    name: "Comprar uma cadeira ergonômica",
+    percentage: "75%",
+    current: "R$ 900,00",
+    target: "R$ 1.200,00",
+  },
+  {
+    id: '3',
+    name: "Fazer uma viagem para o Rio de Janeiro",
+    percentage: "75%",
+    current: "R$ 1.200,00",
+    target: "R$ 3.000,00",
+  },
+]
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text>Open up app/index.tsx</Text>
+    <View style={{ flex: 1 }}>
+      <HomeHeader data={summary} />
+
+      <List
+        title='Metas'
+        data={[]}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Target data={item} />}
+        emptyMessage='Nenhuma meta. Toque em nova meta para criar.'
+        containerStyle={{ paddingHorizontal: 24 }}
+      />
     </View>
   )
 }
---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import { ActivityIndicator } from 'react-native';
+
+import { colors } from '@/theme';
+import { styles } from './styles';
+
+
+export function Loading() {
+  return <ActivityIndicator color={colors.blue[ 500 ]} style={styles.container} />
+}
