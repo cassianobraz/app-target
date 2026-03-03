e61ecffc9dfc298df9e8abaa5920cb52a1d71a0b
cassianobraz <cassanopb@gmail.com>
2026-03-03 00:43:03 -0400

    feat: finish layout mobile

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -5,9 +5,13 @@ import { PageHeader } from "@/components/PageHeader";
 import { CurrencyInput } from "@/components/CurrencyInput";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
+import { TransactionType } from "@/components/TransactionType";
+import { useState } from "react";
+import { TransactionTypes } from "@/utils/TransactionTypes";
 
 export default function Transaction() {
   const params = useLocalSearchParams<{ id: string }>()
+  const [type, setType] = useState(TransactionTypes.Input)
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
@@ -16,6 +20,7 @@ export default function Transaction() {
         subtitle="A cada valor guardado você fica mais próximo da sua meta. Se esforce para guardar e evitar retirar." />
 
       <View style={{ marginTop: 32, gap: 24 }}>
+        <TransactionType selected={type} onChange={setType} />
         <CurrencyInput label="Valor (R$)" value={0} />
         <Input label="Motivo (opcional)" placeholder="Ex: Investir em CDB de 110% no banco XPTO" />
         <Button title="Salvar" onPress={() => { }} />
